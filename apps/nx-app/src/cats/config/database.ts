6fa8ad8cb555a150ac5f4d221024e149611f8bf4
mcarballido <manu.carba96@gmail.com>
2022-08-24 01:01:15 -0300

    add constants for db hosts

--- apps/nx-app/src/cats/config/database.ts
+++ apps/nx-app/src/cats/config/database.ts
@@ -2,11 +2,12 @@ import { Type } from '@nestjs/common';
 
 import { ICatRepository } from '../repositories/cat.repository';
 import { CatInMemoryRepository } from '../repositories/implementations/cat.in-memory.repository';
+import { DATABASE_HOSTS } from '../../shared/constants';
 
 export function provideCatRepositoryClass(): Type<ICatRepository> {
   switch (process.env.DB_HOST) {
-    case 'postgres':
-    case 'mongodb':
+    case DATABASE_HOSTS.POSTGRES:
+    case DATABASE_HOSTS.MONGODB:
     default:
       return CatInMemoryRepository;
   }
---
+++ apps/nx-app/src/shared/constants.ts
@@ -0,0 +1,4 @@
+export enum DATABASE_HOSTS {
+  POSTGRES = 'postgres',
+  MONGODB = 'mongodb',
+}
