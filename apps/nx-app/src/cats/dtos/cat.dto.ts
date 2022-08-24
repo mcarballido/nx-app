e6e8db7bf9d4fa9bdb35b51277f7ca82d13a474f
mcarballido <manu.carba96@gmail.com>
2022-08-24 00:29:50 -0300

    enable strict in production code

--- apps/nx-app/src/cats/dtos/cat.dto.ts
+++ apps/nx-app/src/cats/dtos/cat.dto.ts
@@ -3,4 +3,11 @@ export class CatDTO {
   readonly name: string;
   readonly dateOfBirth: Date;
   readonly weight: number;
+
+  constructor(id: string, name: string, dateOfBirth: Date, weight: number) {
+    this.id = id;
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.weight = weight;
+  }
 }
