52678ffa950b22322e7eb79b44b4b90ee864b22d
mcarballido <manu.carba96@gmail.com>
2022-08-24 00:01:29 -0300

    extend tests of repository layer

--- apps/nx-app/src/cats/repositories/implementations/tests/cat.in-memory.repository.spec.ts
+++ apps/nx-app/src/cats/repositories/implementations/tests/cat.in-memory.repository.spec.ts
@@ -40,6 +40,54 @@ describe('CatInMemoryRepository', () => {
     });
   });
 
+  describe('getById', () => {
+    describe('when sending an ID of an existing cat', () => {
+      it('should get the cat from the list', async () => {
+        const catsList = [
+          new Cat('a1', 'Morgan', new Date('02/02/2022'), 5),
+          new Cat('a2', 'Asha', new Date('03/03/2018'), 15),
+        ];
+        const expectedCat = new Cat('a2', 'Asha', new Date('03/03/2018'), 15);
+        catInMemoryRepository['cats'] = catsList;
+
+        const fetchedCat = await catInMemoryRepository.getById('a2');
+
+        expect(fetchedCat).toEqual(expectedCat);
+      });
+    });
+
+    describe('when sending an ID of a non-existing cat', () => {
+      it('should throw a not found error', async () => {
+        catInMemoryRepository['cats'] = [
+          new Cat('a1', 'Morgan', new Date('02/02/2022'), 5),
+          new Cat('a2', 'Asha', new Date('03/03/2018'), 15),
+        ];
+
+        const resultPromise = catInMemoryRepository.getById('a100');
+
+        await expect(resultPromise).rejects.toThrow(NotFoundException);
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should update the cat in the list', async () => {
+      const catUpdate = new Cat('a2', 'Tiger', new Date('01/01/2020'), 4);
+      const catsList = [
+        new Cat('a1', 'Morgan', new Date('02/02/2022'), 5),
+        new Cat('a2', 'Asha', new Date('03/03/2018'), 15),
+        new Cat('a3', 'Asha', new Date('04/04/2016'), 35),
+      ];
+      const expectedCatList = [catsList[0], catUpdate, catsList[2]]
+      catInMemoryRepository['cats'] = catsList;
+
+      const obtainedCat = await catInMemoryRepository.update('a2', catUpdate);
+
+      expect(catInMemoryRepository['cats']).toEqual(expectedCatList);
+      expect(obtainedCat).toEqual(catUpdate)
+    });
+  });
+
   describe('delete', () => {
     describe('when sending an ID of an existing cat', () => {
       it('should delete the cat from the list', async () => {
@@ -68,6 +116,7 @@ describe('CatInMemoryRepository', () => {
 
         const resultPromise = catInMemoryRepository.delete('a100');
 
+        expect(catInMemoryRepository['cats']).toEqual(catsList);
         await expect(resultPromise).rejects.toThrow(NotFoundException);
       });
     });
